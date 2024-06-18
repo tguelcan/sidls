ab16d64c63482721f10d68f91e2fc596a5ac3cff
Tayfun Gülcan <tayfun.guelcan@rpc-partners.com>
2024-06-18 12:39:06 +0200

    feat: new components

--- preset/button.ts
+++ preset/button.ts
@@ -1,54 +1,56 @@
 export default {
-	button: {
-		"&:hover": {
-			"@apply bg-transparent text-primary/70": {},
-		},
-	},
-	".btn": {
-		"@apply py-2 px-4 rounded-md transition-colors text-sm text-primary/70":
-			{},
-		"&.small": {
-			"@apply py-1 px-2": {},
-		},
+  button: {
+    "&:hover": {
+      "@apply bg-transparent text-primary/70": {},
+    },
+  },
+  ".btn": {
+    "@apply py-2 px-4 rounded-md transition-colors text-sm text-primary/70": {},
+    "&.small": {
+      "@apply py-1 px-2": {},
+    },
 
-		"&.active, &:hover": {
-			"@apply bg-light text-primary disabled:bg-transparent disabled:text-primary/70":
-				{},
-		},
+    "&.active, &:hover": {
+      "@apply bg-light text-primary disabled:bg-transparent disabled:text-primary/70":
+        {},
+    },
 
-		"&.primary": {
-			"@apply bg-primary text-white shadow-sm disabled:bg-primary/70": {},
-			"&:hover": {
-				"@apply bg-primary/90 disabled:bg-primary/70 disabled:text-white":
-					{},
-			},
-		},
-		"&.secondary": {
-			"@apply bg-light shadow-sm": {},
-			"&:hover": {
-				"@apply bg-light/50 disabled:bg-light disabled:text-primary/70":
-					{},
-			},
-		},
-		"&.alert": {
-			"@apply bg-alert text-white shadow-sm disabled:text-white/70": {},
-			"&:hover": {
-				"@apply bg-alert/80 disabled:bg-alert disabled:text-white/70":
-					{},
-			},
-		},
-		"&.bordered": {
-			"@apply ring-1 ring-light ring-inset shadow-sm": {},
-			"&:hover": {
-				"@apply bg-light/50": {},
-			},
-		},
-		"&.cta": {
-			"@apply rounded-full": {},
-		},
-		"&.circle": {
-			"@apply rounded-full flex items-center justify-center p-0 w-9 h-9 shrink-0 overflow-hidden":
-				{},
-		},
-	},
+    "&.primary": {
+      "@apply bg-primary text-white shadow-sm disabled:bg-primary/70": {},
+      "&:hover": {
+        "@apply bg-primary/90 disabled:bg-primary/70 disabled:text-white": {},
+      },
+    },
+    "&.secondary": {
+      "@apply bg-light shadow-sm": {},
+      "&:hover": {
+        "@apply bg-light/50 disabled:bg-light disabled:text-primary/70": {},
+      },
+    },
+    "&.alert": {
+      "@apply bg-alert text-white shadow-sm disabled:text-white/70": {},
+      "&:hover": {
+        "@apply bg-alert/80 disabled:bg-alert disabled:text-white/70": {},
+      },
+    },
+    "&.bordered": {
+      "@apply ring-1 ring-light ring-inset shadow-sm": {},
+      "&.light": {
+        "@apply text-light": {},
+        "&:hover": {
+          "@apply bg-light/10": {},
+        },
+      },
+      "&:hover": {
+        "@apply bg-light/50": {},
+      },
+    },
+    "&.cta": {
+      "@apply rounded-full": {},
+    },
+    "&.circle": {
+      "@apply rounded-full flex items-center justify-center p-0 w-9 h-9 shrink-0 overflow-hidden":
+        {},
+    },
+  },
 };
--- preset/form.ts
+++ preset/form.ts
@@ -1,57 +1,64 @@
 export default {
-	label: {
-		"@apply block text-sm leading-7": {},
-	},
-	"input[type='text'], [type='email'], input[type='password'], input[type='number'], input[type='url'], input[type='date'], input[type='datetime-local'], input[type='month'], input[type='week'], input[type='time'], input[type='search'], input[type='tel'], select, select[multiple], textarea":
-		{
-			"@apply block w-full border-primary/10 disabled:bg-light disabled:placeholder-primary/30 disabled:border-primary/10 disabled:text-primary/30  rounded-md py-1 text-primary ring-0 transition-colors placeholder:text-primary/50 focus:border-primary/25 focus:ring-2 focus:ring-light sm:text-sm sm:leading-6":
-				{},
-			"&.invalid": {
-				"@apply border-alert": {},
-				"+ .text-subtitle": {
-					"@apply border-alert text-alert": {},
-				},
-			},
-			"&.valid": {
-				"@apply border-success": {},
-				"+ .text-subtitle": {
-					"@apply border-success text-success": {},
-				},
-			},
-			"&.search": {
-				"@apply border-0 bg-light": {},
-				"&.text-subtitle": {
-					"@apply border-alert": {},
-				},
-			},
-		},
-	"input[type='radio'], input[type='checkbox']": {
-		"@apply h-4 w-4 border-primary text-primary focus:ring-primary": {},
-	},
-	"input[type='checkbox']": {
-		"@apply rounded": {},
-	},
-	".input-horizontal": {
-		"@apply flex space-x-2 items-start": {},
-		"& label": {
-			"@apply leading-none": {},
-		},
-	},
-	".input-uniform": {
-		"@apply space-y-4 sm:space-y-6": {},
-	},
-	".icon-left": {
-		"@apply pointer-events-none absolute inset-y-0 start-0 flex items-center ps-2 text-primary/50 peer-disabled:pointer-events-none peer-disabled:opacity-50":
-			{},
-	},
-	".icon-right": {
-		"@apply absolute inset-y-0 end-0 flex items-center pe-2 text-primary/50 peer-disabled:pointer-events-none peer-disabled:opacity-50":
-			{},
-	},
-	".input-icon-left": {
-		"@apply ps-8": {},
-	},
-	".input-icon-right": {
-		"@apply pe-8": {},
-	},
+  label: {
+    "@apply block text-sm leading-7": {},
+  },
+  "input[type='text'], [type='email'], input[type='password'], input[type='number'], input[type='url'], input[type='date'], input[type='datetime-local'], input[type='month'], input[type='week'], input[type='time'], input[type='search'], input[type='tel'], select, select[multiple], textarea":
+    {
+      "@apply block w-full border-primary/10 disabled:bg-light disabled:placeholder-primary/30 disabled:border-primary/10 disabled:text-primary/30  rounded-md py-1 text-primary ring-0 transition-colors placeholder:text-primary/50 focus:border-primary/25 focus:ring-2 focus:ring-light sm:text-sm sm:leading-6":
+        {},
+      "&.invalid": {
+        "@apply border-alert": {},
+        "+ .text-subtitle": {
+          "@apply border-alert text-alert": {},
+        },
+      },
+      "&.valid": {
+        "@apply border-success": {},
+        "+ .text-subtitle": {
+          "@apply border-success text-success": {},
+        },
+      },
+      "&.search": {
+        "@apply border-0 bg-light": {},
+        "&.text-subtitle": {
+          "@apply border-alert": {},
+        },
+      },
+    },
+  "input[type='radio'], input[type='checkbox']": {
+    "@apply h-4 w-4 border-primary text-primary focus:ring-0 focus:ring-offset-0":
+      {},
+  },
+  "input[type='checkbox']": {
+    "@apply rounded": {},
+  },
+  ".input-horizontal": {
+    "@apply flex space-x-2 items-start": {},
+    "& label": {
+      "@apply leading-none": {},
+    },
+  },
+  ".input-uniform": {
+    "@apply space-y-4 sm:space-y-6": {},
+  },
+  ".icon-left": {
+    "@apply pointer-events-none absolute inset-y-0 start-0 flex items-center ps-2 text-primary/50 peer-disabled:pointer-events-none peer-disabled:opacity-50":
+      {},
+  },
+  ".icon-right": {
+    "@apply absolute inset-y-0 end-0 flex items-center pe-2 text-primary/50 peer-disabled:pointer-events-none peer-disabled:opacity-50":
+      {},
+  },
+  ".input-icon-left": {
+    "@apply ps-8": {},
+  },
+  ".input-icon-right": {
+    "@apply pe-8": {},
+  },
+
+  // Checklist
+  ".check-list": {
+    "@apply grid grid-cols-[24px_1fr_auto] items-center gap-6 rounded-lg p-4 ring-2 ring-transparent hover:bg-light has-[:checked]:bg-light has-[:checked]:text-primary":
+      {},
+  },
 };
